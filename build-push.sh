6bd86636a34314c1c8bccc7cf2c9eb60580f309e
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2020-04-18 17:39:50 -0400

    added removal of local image before rebuild

--- build-push.sh
+++ build-push.sh
@@ -12,6 +12,8 @@ IMAGE_VERSION=1.0
 # If ERRORR: No space left on device
 # docker rmi -f $(docker images | grep '<none>' | awk '{print $3}')  2>/dev/null
 
+docker rmi -f ${DOCKER_LOGIN}/$IMAGE_NAME:$IMAGE_VERSION
+
 docker build -f Dockerfile -t ${DOCKER_LOGIN}/$IMAGE_NAME:$IMAGE_VERSION .
 docker login --username=${DOCKER_LOGIN} --password ${DOCKER_PWD} ${DOCKER_REGISTRY}
 docker push ${DOCKER_LOGIN}/$IMAGE_NAME:$IMAGE_VERSION
