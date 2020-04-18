00fd5d3392fa570795953833ff29fa542fe0392b
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2020-04-18 19:52:36 -0400

    added imaged cleanup before build

--- build-push.sh
+++ build-push.sh
@@ -2,16 +2,14 @@
 
 set -x
 
-DOCKER_LOGIN=
-DOCKER_PWD=
+# DOCKER_LOGIN=
+# DOCKER_PWD=
 DOCKER_REGISTRY=registry-1.docker.io
 
 IMAGE_NAME=bitnami-tomcat9-jdk18-root-war
 IMAGE_VERSION=1.0
 
-# If ERRORR: No space left on device
-# docker rmi -f $(docker images | grep '<none>' | awk '{print $3}')  2>/dev/null
-
+docker rmi -f $(docker images | grep '<none>' | awk '{print $3}')  2>/dev/null
 docker rmi -f ${DOCKER_LOGIN}/$IMAGE_NAME:$IMAGE_VERSION
 
 docker build -f Dockerfile -t ${DOCKER_LOGIN}/$IMAGE_NAME:$IMAGE_VERSION .
